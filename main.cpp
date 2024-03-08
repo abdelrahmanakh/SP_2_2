335ef0deb64e9a5252d7524a059ef496dd2ce197
abdelrahmanakh <akhteebabdo@gmail.com>
2024-03-08 17:25:44 +0200

    Adding decipher code

--- main.cpp
+++ main.cpp
@@ -16,14 +16,11 @@ string upper(string str){
 bool is_valid_key(string keyword){
     int length = 0;
     for (int i=0;i<keyword.size();i++){
-        if((keyword[i]>=97 && keyword[i]<=122)||(keyword[i]>=65 && keyword[i]<=90)){
-            length += 1;
-        }
-        else{
-            break;
+        if(!((keyword[i]>=97 && keyword[i]<=122)||(keyword[i]>=65 && keyword[i]<=90))){//if character is not between [97,122] and [65,90] then it is not alpha
+            return false;
         }
     }
-    if (length==keyword.size()&&length<=8){ //if length < keyword.size() this means there is at least one non-alphabetic char
+    if (keyword.size()<=8){ //length of keyword exceeds 8 chars
         return true;
     }
     return false;
@@ -46,13 +43,30 @@ string vignere_cipher_encryption(string msg, string keyword){
     }
     return encrypted;
 }
+
+
+string vignere_cipher_decryption(string msg, string keyword){
+    string decrypted;
+    msg = upper(msg);
+    keyword = upper(keyword);
+    //loop through each letter in the message
+    for (int i=0,j=0;i<msg.size();i++,j++){
+        if(msg[i]<65||msg[i]>90){
+            decrypted += char(msg[i]);//if the character is not alphabetic add it to the encrypted message as it is
+            continue;
+        }
+        decrypted += char(((msg[i]-keyword[j]+26)%26)+65);//Vignere DeCipher subtract the ASCII of keyword[i] from msg[i], add 26, then get the modulo of that by 26 add it to 65 the ASCII of 'A'
+        if (j==keyword.size()-1)
+            j=-1;//if we looped through all the letters of the keyword start over
+    }
+    return decrypted;
+}
 //user menu
-void menu() {
-    cout<<"Hello my dear user this is our cipher program, we have:";
+void Cipher_menu() {
     while (true) {
         int choice;
-        cout<<"\n=============================================================";
-        cout<< "\n1)Vignere Cipher\n2)Baconian Cipher\n3)Morse Code\n4)exit\n";
+        cout<<"=============================================================";
+        cout<< "\n1)Vignere Cipher\n2)Baconian Cipher\n3)Morse Code\n4)return\n";
         cin>>choice;
         //user chose exit
         if (choice == 4){
@@ -69,11 +83,11 @@ void menu() {
 
             if(choice==1){//user chose Vignere Cipher
                 if(!is_valid_key(keyword)){
-                    cout<<"please enter only 8 alphabetic letters for the keyword";
+                    cout<<"please enter only 8 alphabetic letters for the keyword"<<endl;
                     continue;
                 }
                 if (msg.size()>80){
-                    cout<<"message size shouldn't be greater than 80 characters, please try again!!";
+                    cout<<"message size shouldn't be greater than 80 characters, please try again!!"<<endl;
                     continue;
                 }
                 encryption = vignere_cipher_encryption(msg, keyword);
@@ -82,12 +96,55 @@ void menu() {
             else if(choice==2){
 
             }
-
             else if(choice==3){
 
             }
+            cout<<encryption<<endl;
+        }
+        else{//unexpected value
+            continue;
+        }
+    }
+}
 
-            cout<<encryption;
+void deCipher_menu(){
+    while (true){
+        int choice;
+        cout<<"=============================================================";
+        cout<< "\n1)Vignere deCipher\n2)Baconian deCipher\n3)Morse deCode\n4)return\n";
+        cin>>choice;
+        //user chose exit
+        if (choice == 4){
+            break;
+        }
+        else if(choice==1||choice==2||choice==3){
+            string msg, keyword, decryption;
+
+            cin.ignore();//ignore the last cin to not conflict with the next cin call
+            cout<<"Enter your message:";
+            getline(cin, msg);//get the whole line with spaces as input
+            cout<<"Enter your keyword:";
+            getline(cin, keyword);//get the whole line with spaces as input
+
+            if(choice==1){//user chose Vignere deCipher
+                if(!is_valid_key(keyword)){
+                    cout<<"please enter only 8 alphabetic letters for the keyword"<<endl;
+                    continue;
+                }
+                if (msg.size()>80){
+                    cout<<"message size shouldn't be greater than 80 characters, please try again!!"<<endl;
+                    continue;
+                }
+                decryption = vignere_cipher_decryption(msg, keyword);
+            }
+
+            else if(choice==2){
+
+            }
+            else if(choice==3){
+
+            }
+            cout<<decryption<<endl;
         }
         else{//unexpected value
             continue;
@@ -96,5 +153,21 @@ void menu() {
 }
 
 int main() {
-    menu();
+    cout << "Hello my dear user this is our cipher program, we have:\n";
+    while (true) {
+        int choice;
+        cout<<"=============================================================";
+        cout << "\n1)Cipher\n2)Decipher\n3)exit\n";
+        cin >> choice;
+        if (choice == 1)
+            Cipher_menu();
+        else if (choice == 2)
+            deCipher_menu();
+        else if (choice == 3)
+            break;
+        else {
+            cout<<"Please enter 1, 2 or 3"<<endl;
+            continue;
+        }
+    }
 }
