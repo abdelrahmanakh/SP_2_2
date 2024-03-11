c7d23026a40da354e72b540c9585bbab193a01bc
PG-Mazen <mazen.2003.pg.business@gmail.com>
2024-03-11 02:12:20 +0200

    I have add Morse code to the cpp file in a new file called modified.cpp

---
+++ cmake-build-debug/CMakeFiles/SP_2_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SP_2_2.dir/main.cpp.o"
+  "CMakeFiles/SP_2_2.dir/main.cpp.o.d"
+  "SP_2_2"
+  "SP_2_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SP_2_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
